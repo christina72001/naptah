6ea077c0b01ba1edc8b1950af3fc21437e690792
mohmdadl <ab2556506@gmail.com>
2023-05-28 00:46:57 +0300

    resolve conflict

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:naptah/My_Plant/last.dart';
 import 'package:naptah/onboardingScreens/onboardingPage.dart';
 import 'package:naptah/shared/blocObserver.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -38,7 +39,7 @@ class MyApp extends StatelessWidget {
       // supportedLocales: Languages.all,
       title: 'Flutter Demo',
       
-      home: OnboardingPage(),
+      home: last(),
     );
     //
   }
